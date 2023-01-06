dbb35a5ba86db660071d35106072f6d3de892bfb
John Tash <johntash@Johns-MacBook-Air.local>
2023-01-06 16:38:32 -0500

    Finalized form

--- assets/submit.js
+++ assets/submit.js
@@ -7,18 +7,7 @@ function sendEmail() {
     const subject = document.getElementById("subject").value;
     const body = document.getElementById("message").value;
     console.log({name, address, subject, body})
-    const alertMessage = "Thank you for contacting John. He will reach back out with a response to your inquiry!"
-    alert(alertMessage);
-    clearForm()
-    // send email using JavaScript
-    // Email.send({
-    //     secureToken: "b541564b-0ae9-4f61-ac6a-5ab001445731",
-    //     To : "jtash25@gmail.com",
-    //     From : address,
-    //     Subject : subject,
-    //     Body : `You Received a message from ${name}. 
-    //     Please find their message text: ${body}`
-    // }).then(alert(alertMessage)).then(clearForm())    
+    clearForm()    
   }
 
   const submitButton = document.getElementById("submit");
